d3d41f2118c1b98119c38525f1fd2a46e5a27342
Marc Boreu <marcboreu@gmail.com>
2020-11-08 18:19:27 +0100

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+const GifExpertApp = () => {
+
+
+    const [categories, setCategories] = useState(['Dragon Ball']);
+
+    // const handleAdd = () => {
+    //     // setCategories( [...categories, "Hunter"]);
+    //     setCategories( cats => [...cats, "Hunter"]);
+
+    // };
+
+
+    return (
+        <>
+        <h2>GifExpertApp</h2>
+        <AddCategory setCategories={setCategories}/>
+        <hr/>
+
+
+
+        <ol>
+            {
+                categories.map(category => (
+                    <GifGrid 
+                        key={category}
+                        category={category}
+                    />
+                ))
+            }
+        </ol>
+
+        </>
+    );
+};
+
+export default GifExpertApp;
